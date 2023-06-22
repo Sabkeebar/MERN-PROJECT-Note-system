2daecbfe2e2b614283450fdc48f9cd92b053d661
Sabkeebar <102631668+ssabkebar17@users.noreply.github.com>
2023-06-22 10:41:53 -0500

    react form has added

--- front-end/notes-system/src/features/notes/notesApiSlice.js
+++ front-end/notes-system/src/features/notes/notesApiSlice.js
@@ -34,11 +34,49 @@ import {
                  } else return [{ type: 'Note', id: 'LIST' }]
              }
          }),
+         addNewNote: builder.mutation({
+            query: initialNote => ({
+                url: '/notes',
+                method: 'POST',
+                body: {
+                    ...initialNote,
+                }
+            }),
+            invalidatesTags: [
+                { type: 'Note', id: "LIST" }
+            ]
+        }),
+        updateNote: builder.mutation({
+            query: initialNote => ({
+                url: '/notes',
+                method: 'PATCH',
+                body: {
+                    ...initialNote,
+                }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'Note', id: arg.id }
+            ]
+        }),
+        deleteNote: builder.mutation({
+            query: ({ id }) => ({
+                url: `/notes`,
+                method: 'DELETE',
+                body: { id }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'Note', id: arg.id }
+            ]
+        }),
+         
      }),
  })
  
  export const {
      useGetNotesQuery,
+     useAddNewNoteMutation,
+    useUpdateNoteMutation,
+    useDeleteNoteMutation,
  } = notesApiSlice
  
  // returns the query result object
